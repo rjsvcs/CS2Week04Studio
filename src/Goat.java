0dd9fd3f710cda01e5945d2b4857769884c1703d
rjsvcs <rjsvcs@rit.edu>
2019-02-03 12:52:34 -0500

    Implemented v1 of the Mage class.

--- src/Goat.java
+++ src/Goat.java
@@ -1,19 +1,60 @@
 import java.util.Random;
 
+/**
+ * The interface that defines behavior common to all Goat characters.
+ */
 public interface Goat {
+    /**
+     * Shared random number generator used to, well, generate random numbers.
+     */
     public static final Random RNG = new Random();
 
+    /**
+     * Helper method that generates a random number in the specified range.
+     * @param min The minimum value (inclusive).
+     * @param max The maximum value (inclusive).
+     * @return The randomly generated number.
+     */
     public static int randomNumber(int min, int max) {
         int bound = max - min + 1;
         int random = RNG.nextInt(bound);
         return random + min;
     }
 
+    /**
+     * Returns the name of the {@link Goat}
+     * @return The {@link Goat Goat's} name.
+     */
     String getName();
 
-    Attack randomAttack();
+    /**
+     * Creates and returns an {@link Attack}.
+     * @return An {@link Attack}
+     */
+    Attack attack();
 
-    void takeDamage(Attack attack);
+    /**
+     * Called when the {@link Goat} has been attacked by an enemy character.
+     *
+     * @param attack The enemy character's {@link Attack}.
+     */
+    void attacked(Attack attack);
 
+    /**
+     * Increases the {@link Goat Goat's} hit points by the specified amount.
+     * The {@link Goat Goat's} hit points should not exceed the maximum.
+     *
+     * @param amount The amount by which the {@link Goat Goat's} hit points
+     *               should be increased.
+     */
     void heal(int amount);
+
+    /**
+     * Returns true if the {@link Goat} has at least 1 hit point remaining.
+     *
+     * @return True if the {@link Goat} has at least 1 hit point remaining,
+     * false if the {@link Goat} has been knocked out.
+     *
+     */
+    boolean isConscious();
 }
--- src/Mage.java
+++ src/Mage.java
@@ -21,27 +21,38 @@ public class Mage implements Goat {
     }
 
     @Override
-    public Attack randomAttack() {
-        int[] hits = new int[randomNumber(3, 5)];
+    public Attack attack() {
+        int[] hits = new int[Goat.randomNumber(3, 5)];
         for(int i=0; i<hits.length; i++) {
-            hits[i] = randomNumber(minAttack, maxAttack);
+            hits[i] = Goat.randomNumber(minAttack, maxAttack);
         }
         return new Attack("Magic Missiles", hits, type);
     }
 
     @Override
-    public void takeDamage(Attack attack) {
+    public void attacked(Attack attack) {
+        double adjust = 1;
+        if(attack.getType() == DamageType.MAGICAL) {
+            adjust = 0.75;
+        } else if(attack.getType() == DamageType.PHYSICAL) {
+            adjust = 1.25;
+        }
 
+        for(int hit : attack.getHits()) {
+            currentHP -= hit;
+        }
+
+        currentHP = currentHP >= 0 ? currentHP : 0;
     }
 
     @Override
     public void heal(int amount) {
-
+        currentHP += amount;
+        currentHP = currentHP <= maxHP ? currentHP : maxHP;
     }
 
-    private int randomNumber(int min, int max) {
-        int bound = max - min + 1;
-        int random = RNG.nextInt(bound);
-        return random + min;
+    @Override
+    public boolean isConscious() {
+        return currentHP > 0;
     }
 }

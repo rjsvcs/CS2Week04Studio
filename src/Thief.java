a73f34d288d390a36d4d1497d8edaa95c9888168
Bobby St. Jacques, Jr <mobappdevguy@gmail.com>
2019-02-05 12:20:25 -0500

    Fixed the Thief class so that its hits can crit.

--- src/Thief.java
+++ src/Thief.java
@@ -14,7 +14,9 @@ public class Thief extends CommonGoat {
     public Attack attack() {
         int[] hits = new int[Goat.randomNumber(MIN_HITS, MAX_HITS)];
         for(int i=0; i<hits.length; i++) {
-            hits[i] = Goat.randomNumber(getMinAttack(), getMaxAttack());
+            int hit = Goat.randomNumber(getMinAttack(), getMaxAttack());
+            int crit = Goat.randomNumber(1, 100);
+            hits[i] = crit > 75 ? hit * 2 : hit;
         }
         return new Attack("Stabbity-Stab", hits, Damage.PHYSICAL);
     }
